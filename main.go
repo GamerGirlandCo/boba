4651b415f6a34783f8e67bb4d272730d3973b2b6
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2023-06-07 18:26:23 -0400

    (re) init go module

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"git.tablet.sh/tablet/boba/datepicker"
+	tea "github.com/charmbracelet/bubbletea"
+	"golang.org/x/term"
+
+	// types "tablet/obsidian-publish/types"
+)
+
+func main() {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	wo, ho, _ := term.GetSize(int(os.Stdout.Fd()))
+	log.Printf("term ix [%dx%d]", wo, ho)
+	
+	p := tea.NewProgram(datepicker.Initialize())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
