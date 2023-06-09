e39975739bc45284c35f5c1cb7dddc1b09257606
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2023-06-09 14:43:15 -0400

    (demo) re-enable mouse

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	wo, ho, _ := term.GetSize(int(os.Stdout.Fd()))
 	log.Printf("term is [%dx%d]", wo, ho)
 
-	p := tea.NewProgram(demo.Setup(), /*  tea.WithMouseCellMotion() */)
+	p := tea.NewProgram(demo.Setup(),  tea.WithMouseCellMotion())
 	m, err := p.Run(); 
 	if err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
