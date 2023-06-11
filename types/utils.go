ccf562d6083e6b968c3773c1649b5147c9e9a109
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2023-06-11 03:21:26 -0400

    update and add utility types

--- types/utils.go
+++ types/utils.go
@@ -3,7 +3,17 @@ package types
 import "time"
 
 type Predicate[T any] func(t T) bool
+
+type IToString interface {
+	ToString() string
+}
+
+type Capsule[T any] struct {
+	value T
+}
+
 type GenResultMsg[T any] struct {
 	Res T
 }
+
 type TickMsg time.Time
