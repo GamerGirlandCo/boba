0a0eb9176b1dbb7fbed38157b0f70b645cfef0a2
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2023-06-19 15:03:53 -0400

    rework demo kinda

--- demo/rlistdemo.go
+++ demo/rlistdemo.go
@@ -6,6 +6,7 @@ import (
 	// "reflect"
 
 	"git.tablet.sh/tablet/boba/recursivelist"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/go-loremipsum/loremipsum"
@@ -20,20 +21,38 @@ var MyOptions recursivelist.Options = recursivelist.Options{
 	Keymap:       recursivelist.DefaultKeys,
 }
 
+type km struct {
+	Check key.Binding
+}
+
+var dkm km = km {
+	Check: key.NewBinding(
+		key.WithKeys("."),
+		key.WithHelp(".", "check/uncheck line"),
+	),
+}
+
 type WrapperModel struct {
-	InnerValue recursivelist.Model[rListItem]
+	InnerValue *recursivelist.Model[rListItem]
 }
 
 func (w WrapperModel) Init() tea.Cmd {
-	return nil
+	return w.InnerValue.Init()
 }
 
 func (w WrapperModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmds []tea.Cmd
-
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, dkm.Check):
+			selit := w.InnerValue.List.SelectedItem().(recursivelist.ListItem[rListItem])
+			selit.Value().Toggle()
+		}
+	}
 	something, cmd := w.InnerValue.Update(msg)
 	cmds = append(cmds, cmd)
-	w.InnerValue = something.(recursivelist.Model[rListItem])
+	*w.InnerValue = something.(recursivelist.Model[rListItem])
 	return w, tea.Batch(cmds...)
 }
 
@@ -43,7 +62,7 @@ func (w WrapperModel) View() string {
 
 var lor loremipsum.LoremIpsum = *loremipsum.NewWithSeed(1234)
 
-func genRandList(par *rListItem, deleg list.ItemDelegate, maxDepth int, curDepth int) ([]recursivelist.ListItem[rListItem], []rListItem) {
+func genRandList(par *rListItem, deleg list.ItemDelegate, maxDepth int, curDepth int, re recursivelist.Model[rListItem]) ([]recursivelist.ListItem[rListItem], []rListItem) {
 	retVal := make([]recursivelist.ListItem[rListItem], 0)
 	secRetVal := make([]rListItem, 0)
 	for i := 0; i < rand.Intn(10)+1; i++ {
@@ -53,21 +72,22 @@ func genRandList(par *rListItem, deleg list.ItemDelegate, maxDepth int, curDepth
 			Name:     lor.Word(),
 			parent:   par,
 			children: &[]rListItem{},
+			checked: rand.Intn(2) == 1,
 		}
 
-		cv := recursivelist.NewItem(cri, deleg)
+		cv := recursivelist.NewItem(cri, deleg, MyOptions, re)
 		if curDepth <= maxDepth {
 			curDepth++
-			sts, fuckery = genRandList(cv.Value(), deleg, maxDepth, curDepth)
+			sts, fuckery = genRandList(cv.Value(), deleg, maxDepth, curDepth, re)
 		}
 		cri.AddMulti(fuckery...)
+			cv.SetChildren(sts)
 		for ii, b := range sts {
 			pari := cv.Value()
+			(*cri.children)[ii] = *b.Value()
 			for _, c := range *cri.children {
 				c.parent = pari
 			}
-			cv.Add(b, ii)
-			// cri
 		}
 		secRetVal = append(secRetVal, *cv.Value())
 		// secRetVal = append(secRetVal, *cv.Value().children...)
@@ -80,10 +100,11 @@ func genRandList(par *rListItem, deleg list.ItemDelegate, maxDepth int, curDepth
 
 func initRlistModel() WrapperModel {
 	MyOptions.SetExpandable(true)
+	nu := recursivelist.New[rListItem]([]rListItem{}, rListDelegate{}, MyOptions)
 	m := WrapperModel{
-		InnerValue: recursivelist.New[rListItem]([]rListItem{}, rListDelegate{}, 500, MyOptions),
+		InnerValue: &nu,
 	}
-	rlisto, _ := genRandList(nil, rListDelegate{}, 6, 0)
+	rlisto, _ := genRandList(nil, rListDelegate{}, 6, 0, nu)
 	m.InnerValue.SetItems(rlisto)
 	return m
 }
